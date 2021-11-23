cd49b4d34b806c2e4aee0b142febe3b2050adf45
MiguelVargas3122 <94325794+MiguelVargas3122@users.noreply.github.com>
2021-11-23 18:38:14 -0500

    Add files via upload

---
+++ 11.OperadoresAritmeticosCombinados/src/operadores/Principal.java
@@ -0,0 +1,25 @@
+
+package operadores;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+       
+        int x = 10;
+        
+    // Convertir "x = x + 5;" mas reducida
+       x += 5; //es equivalente a "x = x + 5;" 
+       
+    // en caso de Resta: "x -= 5;"
+    // en caso de Multiplicacion: "x *= 5;"
+    //en caso de Division: "x /= 5;"
+    
+   //Para imprimir el resultado usamos el "System.out.println();"
+   // y dentro de los parentesis ponemos el nombre de nuestra 
+   //variable en este caso se llama: "x"
+        System.out.println(x);
+        
+    }
+    
+}
